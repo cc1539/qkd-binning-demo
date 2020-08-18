0017a5f3147c016ea647e01565033342474ec47d
unknown <metash.ian@gmail.com>
2020-08-18 17:22:59 -0400

    progress
    Co-Authored-By: cc1539 <cc1539@users.noreply.github.com>

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 
+function setup() {
+	createCanvas(840,640);
+	let canvas = document.getElementById("defaultCanvas0");
+	document.getElementById("canvas-holder").appendChild(canvas);
+}
+
+function draw() {
+	background(255);
+}
