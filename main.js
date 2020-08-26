832e0ee86007e2545bd959692983c78bd6fcc3c9
unknown <metash.ian@gmail.com>
2020-08-26 14:31:31 -0400

    control transferred to drop-down

--- main.js
+++ main.js
@@ -143,6 +143,9 @@ let graphUpdateInterval = null;
 let graphControlPanel;
 let controlTemplate;
 
+let xAxisMode = 0;
+let yAxisMode = 0;
+
 async function updateGraphs() {
 	
 	if(graphUpdateInterval!=null) {
@@ -165,7 +168,7 @@ async function updateGraphs() {
 				bins[i][j].getOutput().read();
 			}
 			
-			let h = (keyIsPressed&&keyCode==CONTROL)?1:entropy(p);
+			let h = yAxisMode==1?1:entropy(p);
 			rateGraphs[i][j] = h==0?0:(bins[i][j].getRawKeyRate()/h);
 			randGraphs[i][j] = bins[i][j].getAnalysis().getRandomness();
 		}
@@ -325,6 +328,10 @@ function setup() {
 		}
 	};
 	
+	document.querySelector('select[name="y-axis"]').onchange = function() {
+		yAxisMode = this.selectedIndex;
+	};
+	
 }
 
 function keyTyped() {
@@ -373,13 +380,13 @@ function draw() {
 	let h = height-border*2;
 	grid(x,y,w,h,w*.1*graphScaleX,h*.1*graphScaleY,.1,.1);
 	labels(x,y,w,h,"n="+frameSize,"Probability (p)",
-		(keyIsPressed&&keyCode==SHIFT)?"Randomness (H(X)h(p))":
-		(keyIsPressed&&keyCode==CONTROL)?"Raw Key Rate (r)":
+		yAxisMode==2?"Randomness (H(X)h(p))":
+		yAxisMode==1?"Raw Key Rate (r)":
 		"Photon Utilization (r/h(p))");
 	for(let i=0;i<rateGraphs.length;i++) {
 		noFill();
 		stroke(getColorPicker(i).value);
-		plot((keyIsPressed&&keyCode==SHIFT)?randGraphs[i]:rateGraphs[i],
+		plot(yAxisMode==2?randGraphs[i]:rateGraphs[i],
 				border+1,h+border-1,w*graphScaleX-2,h*graphScaleY-2);
 	}
 	
